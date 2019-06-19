f538923238090099502c6c222be8266aeaca7bd7
hectorscreativeworld <hectorscreativeworld@gmail.com>
2019-06-18 23:33:07 -0400

    mathapi

---
+++ Controllers/ViewModels/MathResult.cs
@@ -0,0 +1,10 @@
+// namespace MathApi.Controllers.ViewModels
+// {
+//   public class MathResult
+//   {
+//     public int Results { get; set; }
+//     public int X { get; set; }
+//     public int Y { get; set; }
+//     public string operation { get; set; }
+//   }
+// }
